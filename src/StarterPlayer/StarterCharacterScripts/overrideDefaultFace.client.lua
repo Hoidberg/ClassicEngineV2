0af79d390b3514339a57a51585e3252a1e2a7a74
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-08-26 16:51:42 -0500

    Make luacheck happy

--- src/StarterPlayer/StarterCharacterScripts/overrideDefaultFace.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/overrideDefaultFace.client.lua
@@ -1,10 +1,11 @@
 local RunService = game:GetService("RunService")
 
 RunService.RenderStepped:Connect(function()
-	if script.Parent:IsA("Model") and script.Parent:FindFirstChild("Humanoid") then -- check if the script is in the players character
-		for i, child in ipairs(script.Parent.Head:GetChildren()) do
-			if child:IsA("Decal") and child.Texture == "rbxasset://textures/face.png" then
-				child.Texture = "rbxassetid://1104210678"
+	-- check if the script is in the players character
+	if script.Parent:IsA("Model") and script.Parent:FindFirstChild("Humanoid") then
+		for _,v in ipairs(script.Parent.Head:GetChildren()) do
+			if v:IsA("Decal") and v.Texture == "rbxasset://textures/face.png" then
+				v.Texture = "rbxassetid://1104210678"
 			end
 		end
 	end
