c8110573e95750c72aa6387ecc013caf3f948fec
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-08-26 16:48:56 -0500

    Update deletemesh.client.lua

--- src/StarterPlayer/StarterCharacterScripts/deletemesh.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/deletemesh.client.lua
@@ -3,9 +3,9 @@ local RunService = game:GetService("RunService")
 RunService.RenderStepped:Connect(function()
 	-- check if the script is in the players character
 	if script.Parent:IsA("Model") and script.Parent:FindFirstChild("Humanoid") then
-		for i, child in ipairs(script.Parent:GetChildren()) do
-			if child:IsA("CharacterMesh") then
-				child:Destroy()
+		for _,v in ipairs(script.Parent:GetChildren()) do
+			if v:IsA("CharacterMesh") then
+				v:Destroy()
 			end
 		end
 		script:Destroy()
