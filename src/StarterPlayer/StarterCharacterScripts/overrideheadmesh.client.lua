a66b50eb72064d299beaf15a55338550fbd444b7
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-08-26 18:06:13 -0500

    Update overrideheadmesh.client.lua

--- src/StarterPlayer/StarterCharacterScripts/overrideheadmesh.client.lua
+++ src/StarterPlayer/StarterCharacterScripts/overrideheadmesh.client.lua
@@ -1,7 +1,8 @@
 local RunService = game:GetService("RunService")
 
 RunService.RenderStepped:Connect(function()
-	if script.Parent:IsA("Model") and script.Parent:FindFirstChild("Humanoid") and script.Parent:FindFirstChild("Head") then -- check if the script is in the players character
+	-- check if the script is in the players character
+	if script.Parent:IsA("Model") and script.Parent:FindFirstChild("Humanoid") and script.Parent:FindFirstChild("Head") then
 		script.Parent.Head:FindFirstChildWhichIsA("FileMesh").MeshId = "rbxassetid://1104623876"
 	end
 end)
